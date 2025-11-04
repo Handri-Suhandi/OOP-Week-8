f252ca0384cc59710334cb1c0d5ecdfa062d802d
Handri Suhandi <Handri>
2025-11-04 18:56:35 +0700

    Main class

--- src/week8/Main.java
+++ src/week8/Main.java
@@ -1,17 +1,54 @@
 package week8;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    static ArrayList<Item> ListOfItems = new ArrayList<Item>();
+    static ArrayList<Payment> ListOfPayments = new ArrayList<Payment>();
+    static Scanner s = new Scanner(System.in);
+
+    public static void seedData(){
+        ListOfItems.add(new Item("Kulkas", "Elektronik", 4800000));
+        ListOfItems.add(new Item("TV", "Elektronik", 1280000));
+        ListOfItems.add(new Item("Laptop", "Komputer", 6000000));
+        ListOfItems.add(new.Item("PC", "Komputer", 12000000));
+    }
+
+    public static void printItem(Item item){
+        System.out.printf("Nama     : "+item.getName());
+        System.out.printf("Tipe     : "+item.getType());
+        System.out.printf("Harga    : "+item.getPrice());
+    }
+
+    public static void main(String[] args) {
+        int opt = 0;
+        int id = 0;
+        seedData();
+        do{
+            System.out.printf("---Program Toko Elektronik---");
+            System.out.println("1. Pesan Barang");
+            System.out.println("2. Lihat Pesanan");
+            System.out.println("0. Keluar");
+            opt = s.nextInt();
+            if(opt == 1){
+                System.out.println("---Daftar Barang---");
+                for(int i = 0; i<ListOfItems.size(); i++){
+                    System.out.println("No  : "+(i+1));
+                    printItem(ListOfItems.get(i));
+                    System.out.println("-------------------");
+                }
+                System.out.println("Pilih   : ");
+            }
+            else if (opt == 2) {
+
+            }
+            else if (opt == 0) {
+
+            }
+            else{
+                System.out.println("Salah Input");
+            }
+        }while (opt != 0);
     }
 }
